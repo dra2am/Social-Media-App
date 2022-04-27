050b8afb278485efae94042a35cb3e8d0edef396
Arantxa Edwards <37158609+arantxa-e@users.noreply.github.com>
2022-04-27 19:42:51 -0400

    Add UI to product details page

--- pages/products/details/[product_id].js
+++ pages/products/details/[product_id].js
@@ -1,5 +1,8 @@
 import { useRouter } from "next/router";
+import { Container, Row, Col, Button } from "react-bootstrap";
 import products from "../data";
+import TopNav from "../../../components/TopNav";
+import SelectQuantity from "../../../components/products/select_quantity";
 
 const details = () => {
     const router = useRouter();
@@ -7,9 +10,34 @@ const details = () => {
     const product = products.find(product => product.id === +product_id);
 
     return (
-        <h1>
-            {product?.name}
-        </h1>
+        <>
+            <TopNav />
+            <Container>
+                <Row>
+                    <Col xs={4}>
+                        <img src={product?.img} className="img-fluid" alt={product?.name} />
+                    </Col>
+                    <Col xs={8}>   
+                        <Row>
+                            <Col>
+                                <h1>
+                                    {product?.name}
+                                </h1>
+                                <span className="text-muted">
+                                    ${product?.price}
+                                </span>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col className="d-flex align-items-center">
+                                <SelectQuantity />
+                                <Button variant="primary">Add to Cart</Button>
+                            </Col>
+                        </Row>
+                    </Col>
+                </Row>
+            </Container>
+        </>
     )
 }
 
