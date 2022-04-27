4a77db300517a3fb64f27f0a5d4ff30dbedddad3
Arantxa Edwards <37158609+arantxa-e@users.noreply.github.com>
2022-04-27 19:42:01 -0400

    Implement basic styles

--- components/TopNav.js
+++ components/TopNav.js
@@ -3,13 +3,13 @@ import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
 
 const TopNav = () => {
   return (
-    <Navbar bg="light" expand="lg" className="mb-4">
+    <Navbar bg="primary" variant="dark" expand="lg" className="mb-4">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home" className="title">All Curls</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#products">Products</Nav.Link>
             <Nav.Link href="#link">Link</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
