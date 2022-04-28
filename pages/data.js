fdf99f65a2fa3500377154922c4840c1249e43da
Arantxa Edwards <37158609+arantxa-e@users.noreply.github.com>
2022-04-27 20:42:38 -0400

    Hook up images to data

--- pages/data.js
+++ pages/data.js
@@ -1,51 +1,66 @@
+import product_1 from "../public/product-1.jpg";
+import product_2 from "../public/product-2.jpg";
+import product_3 from "../public/product-3.jpg";
+import product_4 from "../public/product-4.jpg";
+import product_5 from "../public/product-5.jpg";
+import product_6 from "../public/product-6.jpg";
+import product_7 from "../public/product-7.jpg";
+import product_8 from "../public/product-8.jpg";
+import product_9 from "../public/product-9.jpg";
 
 const products = [
     {   
         id: 1,
         name: "Super Conditioning Conditioner",
-        img: "https://via.placeholder.com/150",
+        img: product_1,
         price: "19.00"
     },
     {
         id: 2, 
         name: "Fantabulous Shampoo",
-        img: "https://via.placeholder.com/150",
+        img: product_2,
         price: "10.99"
     },
     {
         id: 3,
         name: "Super-duper Leave-in Conditioner",
-        img: "https://via.placeholder.com/150",
+        img: product_3,
         price: "14.50"
     },
     {
         id: 4,
         name: "Thirsty Girl Hair Mask",
-        img: "https://via.placeholder.com/150",
+        img: product_4,
         price: "34.60"
     },
     {   
         id: 5,
         name: "Super Conditioning Conditioner",
-        img: "https://via.placeholder.com/150",
+        img: product_5,
         price: "19.00"
     },
     {
         id: 6, 
         name: "Fantabulous Shampoo",
-        img: "https://via.placeholder.com/150",
+        img: product_6,
         price: "10.99"
     },
     {
         id: 7,
         name: "Super-duper Leave-in Conditioner",
-        img: "https://via.placeholder.com/150",
+        img: product_7,       
         price: "14.50"
     },
     {
         id: 8,
         name: "Thirsty Girl Hair Mask",
-        img: "https://via.placeholder.com/150",
+        img: product_8,          
+        price: "34.60"
+    },
+    {
+        id: 8,
+        name: "Thirsty Girl Hair Mask",
+        img: product_9,         
         price: "34.60"
     },
 ]
